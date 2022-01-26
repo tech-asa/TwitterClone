7bcff6ae2ca3f3119e2ab8d1aafc10cf30e9c191
tech-asa <tech-asai07@gmail.com>
2022-01-26 16:33:27 +0900

    phpの関数代入

--- Views/home.php
+++ Views/home.php
@@ -2,13 +2,13 @@
 // エラー表示あり
 ini_set('display_errors',1);
 // 日本時間にする
-date_default_timezone_get('Asia/Tokyo');
+date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL','/TwitterClone/');
 
 // ツイート一覧
 // 配列を入れる（関数$の中に[]で配列を入れる）
-$view_tweeets = [
+$view_tweets = [
     [
         'user_id' => 1,
         'user_name' => 'taro',
@@ -41,10 +41,10 @@ $view_tweeets = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../Views/img/logo-twitterblue.svg">
+    <link rel="icon" href="<?php echo HOME_URL; ?>Views/img/logo-twitterblue.svg">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <link href="../Views/css/style.css" rel="stylesheet">
+    <link href="<?php echo HOME_URL; ?>Views/css/style.css" rel="stylesheet">
     
     <title>ホーム画面 / Twitterクローン</title>
     <meta name="description" content="ホーム画面です">
@@ -54,13 +54,13 @@ $view_tweeets = [
         <div class="side">
             <div class="side-inner">
                 <ul class="nav flex-column">
-                <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
-                <li class="nav-item"><a href="home.php" class="nav-link"><img src="../Views/img/icon-home.svg" alt=""></a></li>
-                <li class="nav-item"><a href="search.php" class="nav-link"><img src="../Views/img/icon-search.svg" alt=""></a></li>
-                <li class="nav-item"><a href="notification.php" class="nav-link"><img src="../Views/img/icon-notification.svg" alt=""></a></li>
-                <li class="nav-item"><a href="profile.php" class="nav-link"><img src="../Views/img/icon-profile.svg" alt=""></a></li>
-                <li class="nav-item"><a href="post.php" class="nav-link"><img src="../Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
-                <li class="nav-item my-icon"><img src="../Views/img_uploaded/user/sample-person.jpg" alt=""></li>
+                <li class="nav-item"><a href="home.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/logo-twitterblue.svg" alt="" class="icon"></a></li>
+                <li class="nav-item"><a href="home.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-home.svg" alt=""></a></li>
+                <li class="nav-item"><a href="search.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-search.svg" alt=""></a></li>
+                <li class="nav-item"><a href="notification.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-notification.svg" alt=""></a></li>
+                <li class="nav-item"><a href="profile.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-profile.svg" alt=""></a></li>
+                <li class="nav-item"><a href="post.php" class="nav-link"><img src="<?php echo HOME_URL; ?>Views/img/icon-post-tweet-twitterblue.svg" alt="" class="post-tweet"></a></li>
+                <li class="nav-item my-icon"><img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt=""></li>
                 </ul>
             </div>
         </div>
@@ -74,7 +74,7 @@ $view_tweeets = [
             <!-- つぶやき投稿エリア -->
             <div class="tweet-post">
                 <div class="my-icon">
-                    <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
+                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/sample-person.jpg" alt="">
                 </div>
                 <!-- 投稿部分 -->
                 <div class="input-area">
@@ -100,54 +100,51 @@ $view_tweeets = [
             <div class="ditch"></div>
 
             <!-- つぶやき一覧エリア -->
-            <div class="tweet-list">
-                <div class="tweet">
-                    <div class="user">
-                        <a href="profile.php?user_id=1">
-                            <img src="../Views/img_uploaded/user/sample-person.jpg" alt="">
-                        </a>
-                    </div>
-                    <div class="content">
-                        <div class="name">
-                            <a href="profile.php?user_id=1">
-                                <span class="nickname">太郎</span>
-                                <span class="user-name">@taro・２３日前</span>
-                            </a>
-                        </div>
-                        <p>今プログラミングをしています。</p>
-                        <div class="icon-list">
-                            <div class="like">
-                                <img src="../Views/img/icon-heart.svg" alt="">
+            <!-- 真の場合 -->
+            <?php if(empty($view_tweets)):?>
+                <!-- 下記の”p-3”はBootstrapの関数 -->
+                <p class="p-3">ツイートがありません</p>
+            <!-- 偽(ツイートがある場合) -->
+            <?php else:?>
+                <div class="tweet-list">
+                <!-- foreachを使って上の配列を組み込む -->
+                    <?php foreach($view_tweets as $view_tweet): ?> 
+                        <!-- この場合asで配列を$view_tweetに組み込んだ
+                        $view_tweet部分がもし $00 => $11 だった場合は 「配列名」と「その値」を使うことができる -->
+                        <div class="tweet">
+                            <div class="user">
+                            <a href="profile.php?user_id = <?php echo $view_tweet['user_id']; ?>">
+                                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/user/<?php echo $view_tweet['user_image_name']; ?>" alt="">
+                                </a>
                             </div>
-                            <div class="like-count">0</div>
-                        </div>
-                    </div>
-                </div>
+                            <div class="content">
+                                <div class="name">
+                                    <a href="profile.php?user_id=<?php echo $view_tweet['user_id']; ?>">
+                                        <span class="nickname"><?php echo $view_tweet['user_nickname']; ?></span>
+                                        <span class="user-name">@<?php echo $view_tweet['user_name']; ?>・<?php echo $view_tweet['tweet_created_at']; ?></span>
+                                    </a>
+                                </div>
+                                <p><?php echo $view_tweet['tweet_body']; ?></p>
 
-                <div class="tweet">
-                    <div class="user">
-                        <a href="profile.php?user_id=2">
-                            <img src="../Views/img/icon-default-user.svg" alt="">
-                        </a>
-                    </div>
-                    <div class="content">
-                        <div class="name">
-                            <a href="profile.php?user_id=2">
-                                <span class="nickname">次郎</span>
-                                <span class="user-name">@jiro・２４日前</span>
-                            </a>
-                        </div>
-                        <p>コワーキングスペースをオープンしました。</p>
-                        <img src="../Views/img_uploaded/tweet/sample-post.jpg" alt="" class="post-image">
-                        <div class="icon-list">
-                            <div class="like">
-                                <img src="../Views/img/icon-heart-twitterblue.svg" alt="">
+                                <!-- isset関数は()の中に値が入っていた場合に真を返す値でNULLが入ってなければ基本的に真 -->
+                                <?php if (isset($view_tweet['tweet_image_name'])) : ?>
+                                    <img src="<?php echo HOME_URL; ?>Views/img_uploaded/tweet/<?php echo $view_tweet['tweet_image_name']; ?>" alt="" class="post-image">
+                                <?php endif; ?>
+
+                                <div class="icon-list">
+                                    <div class="like">
+                                        <!-- いいね -->
+                                        <img src="<?php echo HOME_URL; ?>Views/img/icon-heart.svg" alt="">
+                                    </div>
+                                    <div class="like-count">0</div>
+                                </div>
                             </div>
-                            <div class="like-count">1</div>
                         </div>
-                    </div>
+                    <!-- foreach文の最後を示す -->    
+                    <?php endforeach; ?>
                 </div>
-            </div>
+            <!-- if文の最後を示す(；を使う構文の場合) -->
+            <?php endif;?>
         </div>
     </div>
 </body>
