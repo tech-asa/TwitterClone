e4f9d79f257f42d96662a95c55b9f5b916fe8fc4
tech-asa <tech-asai07@gmail.com>
2022-01-26 11:35:43 +0900

    phpの追加

--- Views/home.php
+++ Views/home.php
@@ -1,3 +1,40 @@
+<?php
+// エラー表示あり
+ini_set('display_errors',1);
+// 日本時間にする
+date_default_timezone_get('Asia/Tokyo');
+// URL/ディレクトリ設定
+define('HOME_URL','/TwitterClone/');
+
+// ツイート一覧
+// 配列を入れる（関数$の中に[]で配列を入れる）
+$view_tweeets = [
+    [
+        'user_id' => 1,
+        'user_name' => 'taro',
+        'user_nickname' => '太郎',
+        'user_image_name' => 'sample-person.jpg',
+        'tweet_body' => '今プログラミングをしています。',
+        'tweet_image_name' => null,
+        'tweet_created_at' => '2021-03-15 14:00:00',
+        'like_id' => null,
+        'like_count' => 0,
+    ],
+    [
+        'user_id' => 2,
+        'user_name' => 'jiro',
+        'user_nickname' => '次郎',
+        'user_image_name' => null,
+        'tweet_body' => 'コワーキングスペースを作成しました!',
+        'tweet_image_name' => 'sample-post.jpg',
+        'tweet_created_at' => '2021-03-14 14:00:00',
+        'like_id' => 1,
+        'like_count' => 1,
+    ],
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
