5fad49d1a5131ab5d3f3215a2dbac51dbf5e417c
tech-asa <tech-asai07@gmail.com>
2022-02-16 14:27:17 +0900

    通知画面にモデルのデータを組み込む

--- Models/notifications.php
+++ Models/notifications.php
@@ -50,3 +50,59 @@ function createNotification(array $data)
  
     return $response;
 }
+
+/**
+ * 通知の一覧を取得
+ * 
+ * @param int $user_id
+ * @return array|false
+ */
+function findNotifications(int $user_id)
+{
+    // DB接続
+    $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    if ($mysqli->connect_errno) {
+        echo 'MySQLの接続に失敗しました。：' . $mysqli->connect_error . "\n";
+        exit;
+    }
+
+    //エスケープ
+    $user_id = $mysqli->real_escape_string($user_id);
+
+    // ------------------------------------
+    // SQLクエリを作成（検索）
+    // ------------------------------------
+    $query = <<<SQL
+        SELECT
+            -- 通知
+            N.id AS notification_id,
+            N.message AS notification_message,
+            -- ユーザー
+            U.name AS user_name,
+            U.nickname AS user_nickname,
+            U.image_name AS user_image_name
+        FROM
+            notifications AS N
+            JOIN
+                users AS U ON U.id = N.sent_user_id AND U.status = 'active'
+        WHERE
+            N.status = 'active' AND N.received_user_id = '$user_id'
+        ORDER BY
+            N.create_at DESC
+        LIMIT 50
+    SQL;
+
+    // ------------------------------------
+    // 戻り値を作成
+    // ------------------------------------
+    // クエリを実行し、SQLエラーでない場合
+    if ($result = $mysqli->query($query)) {
+        // 全件取得
+        $notifications = $result->fetch_all(MYSQLI_ASSOC);
+    } else {
+        $notifications =false;
+        echo 'エラーメッセージ:' . $mysqli->error . "\n";
+    }
+
+    return $notifications;
+}
