d66475c2a38f86da02deca0e0d99d9beecf519f4
tech-asa <tech-asai07@gmail.com>
2022-06-08 15:23:06 +0900

    DB接続

--- config.php
+++ config.php
@@ -6,7 +6,9 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL','/');
 // データベースの接続状況
-define('DB_HOST','localhost'); //例： echo DB_HOST で localhost が出力される
-define('DB_USER','root');
-define('DB_PASSWORD','root');
-define('DB_NAME','twitter_clone');
+define('DB_HOST','us-cdbr-east-05.cleardb.net'); //例： echo DB_HOST で localhost が出力される
+define('DB_USER','b5ef6c01b244eb');
+define('DB_PASSWORD','b5fa5a09');
+define('DB_NAME','heroku_ba0a4750e6f509d');
+
+mysql://b5ef6c01b244eb:b5fa5a09@us-cdbr-east-05.cleardb.net/heroku_ba0a4750e6f509d?reconnect=true
