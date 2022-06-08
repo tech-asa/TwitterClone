5d27a774fca9313ca9196406e356474484b51390
tech-asa <tech-asai07@gmail.com>
2022-06-08 15:23:25 +0900

    DB接続

--- config.php
+++ config.php
@@ -11,4 +11,4 @@ define('DB_USER','b5ef6c01b244eb');
 define('DB_PASSWORD','b5fa5a09');
 define('DB_NAME','heroku_ba0a4750e6f509d');
 
-mysql://b5ef6c01b244eb:b5fa5a09@us-cdbr-east-05.cleardb.net/heroku_ba0a4750e6f509d?reconnect=true
+// mysql://b5ef6c01b244eb:b5fa5a09@us-cdbr-east-05.cleardb.net/heroku_ba0a4750e6f509d?reconnect=true
