54c19c5fdce7a26f4049f0d8ae4570153f82bffa
tech-asa <tech-asai07@gmail.com>
2022-06-10 15:32:44 +0900

    define指定

--- config.php
+++ config.php
@@ -6,9 +6,7 @@ date_default_timezone_set('Asia/Tokyo');
 // URL/ディレクトリ設定
 define('HOME_URL','/');
 // データベースの接続状況
-define('DB_HOST','us-cdbr-east-05.cleardb.net'); //例： echo DB_HOST で localhost が出力される
-define('DB_USER','b5ef6c01b244eb');
-define('DB_PASSWORD','b5fa5a09');
-define('DB_NAME','heroku_ba0a4750e6f509d');
-
-// mysql://b5ef6c01b244eb:b5fa5a09@us-cdbr-east-05.cleardb.net/heroku_ba0a4750e6f509d?reconnect=true
+define('DB_HOST', getenv('DB_HOST'));
+define('DB_USER', getenv('DB_USER'));
+define('DB_PASSWORD', getenv('DB_PASSWORD'));
+define('DB_NAME', getenv('DB_NAME'));
